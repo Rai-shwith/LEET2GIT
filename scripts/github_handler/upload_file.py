fa85e849b1114bde7adfcfd61ef2e09f8aaa22a9
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-12 10:26:43 +0000

    file_name->file_name_with_path

--- scripts/github_handler/upload_file.py
+++ scripts/github_handler/upload_file.py
@@ -1,17 +1,17 @@
 from scripts.logging_config import logger
 from github import Github,Repository
 
-def upload_file(repo:Repository.Repository, file_name:str,content:str, commit_message:str):
+def upload_file(repo:Repository.Repository, file_name_with_path:str,content:str, commit_message:str):
     """
-    Uploads a file to the repository
+    Uploads a file to the perticular directory in the repository
     """
-    logger.info(f"Uploading {file_name} to the repository")
+    logger.info(f"Uploading {file_name_with_path} to the repository")
     try:
-        contents_info = repo.get_contents(file_name)
-        logger.info(f"{file_name} already exists. Updating the file")
+        contents_info = repo.get_contents(file_name_with_path)
+        logger.info(f"{file_name_with_path} already exists. Updating the file")
         repo.update_file(contents_info.path, commit_message, content, contents_info.sha)
-        logger.info(f"{file_name} updated successfully")
+        logger.info(f"{file_name_with_path} updated successfully")
     except:
-        logger.info(f"{file_name}  Creating a new file")
-        repo.create_file(file_name, commit_message, content)
-        logger.info(f"{file_name} created successfully")
+        logger.info(f"{file_name_with_path}  Creating a new file")
+        repo.create_file(file_name_with_path, commit_message, content)
+        logger.info(f"{file_name_with_path} created successfully")
