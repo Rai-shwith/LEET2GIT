011f32f8d85aa48674d990c157640ed2e7cf6b64
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-12 10:19:24 +0000

    Provided Default discription for Created Folders

--- scripts/github_handler/create_repo.py
+++ scripts/github_handler/create_repo.py
@@ -2,6 +2,8 @@ from scripts.logging_config import logger
 from github import Github,AuthenticatedUser,Repository
 
 def create_repo(user:AuthenticatedUser,repo_name:str,description:str="",private:bool=True)->Repository.Repository:
+    if description=="":
+        description="Repository created by LEET2GIT for storing LeetCode solutions"
     logger.info(f"Creating repository {repo_name}")
     repo = user.create_repo(repo_name,description=description,private=private)
     logger.info(f"Repository {repo_name} created successfully")
