ee2423adb1435363379f1202283d37e43aef65fa
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-12 11:37:17 +0000

    Added Users schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class ProblemDetails(BaseModel):
     questionTitle: str
@@ -11,3 +12,14 @@ class ProblemDetails(BaseModel):
 class Solution(BaseModel):
     code_extension: str
     code: str
+    
+class Users(BaseModel):
+    id : int
+    user_name : str
+    email : str
+    password : str
+    github_id : str
+    access_token : str
+    folder_name : str
+    created_at : datetime
+    updated_at : datetime
