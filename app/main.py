5756604438c3b78f618fa7b4a29a0ab54491428a
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-12 11:51:31 +0000

    Included the router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
+from .routers import users
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+app.include_router(users.router)
