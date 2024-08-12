ceb6b4a5e193f5b6fd358f4c31193cf2e958154f
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-12 08:43:48 +0000

    Created a not null folder_name in db

--- app/models.py
+++ app/models.py
@@ -11,5 +11,6 @@ class Users(Base):
     password = Column(String(255),nullable=False)
     github_id = Column(BigInteger,nullable=False)
     access_token = Column(Text,nullable=True)
+    folder_name = Column(String(255), nullable=False, default='LeetCode')
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'), onupdate=text('now()'))
