b027bac1b598ad0b5b65c0dba69b0d158899cff8
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-13 11:00:23 +0000

    changed fileHandling mode of logger to write mode

--- app/routers/logging_config.py
+++ app/routers/logging_config.py
@@ -3,6 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
-file_handler = logging.FileHandler("LOGS/oauth2.log")
+file_handler = logging.FileHandler("LOGS/oauth2.log","w")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
