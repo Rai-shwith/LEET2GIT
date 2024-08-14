e0ee8fc11bf8b119163eee77525dc3c25ed06aa5
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-14 10:34:37 +0000

    Improved

--- scripts/github_handler/get_user_info.py
+++ scripts/github_handler/get_user_info.py
@@ -6,9 +6,11 @@ def get_user_info(access_token)->AuthenticatedUser:
     Get the user information
     """
     logger.info("Getting user information")
+    logger.info(f"Access token: {access_token}")
     try:
         g = Github(access_token)
         user = g.get_user()
+        logger.info(f"User information: {user}")
         return user
     except Exception as e:
         logger.error(f"Failed to get user information: {str(e)}")
