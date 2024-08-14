887bb68274a4f49de8930381fe76f38d689119d7
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-14 07:17:11 +0000

    added filename to logging details

--- app/routers/logging_config.py
+++ app/routers/logging_config.py
@@ -2,7 +2,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s')
 file_handler = logging.FileHandler("LOGS/oauth2.log","w")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
