cee2f799ce2bb188208bf45d5814feea86eb3b01
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-14 06:02:08 +0000

    added create user function

--- app/routers/oauth2.py
+++ app/routers/oauth2.py
@@ -1,12 +1,14 @@
 from fastapi import Depends, HTTPException,status,Request
 import requests
 from .logging_config import logger  
-from ..schemas import GithubUser
+from .. import schemas,models
 from pydantic import ValidationError
+from ..database import get_db
+from sqlalchemy.orm import Session
 
 GITHUB_API_URL = "https://api.github.com/user"
 
-def get_current_user(request:Request)-> GithubUser:
+def get_current_user(request:Request,db:Session=Depends(get_db))-> schemas.Users:
     logger.info("Verifying token")
     token = request.cookies.get("access_token")
     headers = {"Authorization": f"bearer {token}"}
@@ -18,8 +20,24 @@ def get_current_user(request:Request)-> GithubUser:
     logger.info("Token verified")
     logger.info(f"User: {response.json()}")
     try:
-        user = GithubUser(**response.json())
+        github_user = schemas.GithubUser(**response.json())
     except ValidationError as e:
         logger.error(f"Validation error: {e}")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to retrieve user")
+    user = db.query(models.Users).filter(models.Users.github_id == github_user.id).first()
+    if not user:
+        user = create_user(github_user)
+    else:
+        user = schemas.Users.model_validate(user)
     return user
+
+
+def create_user(github_user: schemas.GithubUser,db:Session = Depends(get_db))-> schemas.Users:
+    try:
+        new_user = models.Users(user_name=github_user.login,email=github_user.email,github_id=github_user.id,avatar_url=github_user.avatar_url)
+        db.add(new_user)
+        db.commit()
+    except Exception as e:
+        return {"message":"User already exists"}
+    db.refresh(new_user)
+    return schemas.Users.model_validate(new_user)
