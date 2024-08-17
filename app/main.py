c4931b6cd9b533e77bec7972eac6cc9ff3e61f3b
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-17 07:04:43 +0000

    Mounted static

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
 from .routers import users,auth,post,upload
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -13,3 +15,5 @@ app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(post.router)
 app.include_router(upload.router)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
