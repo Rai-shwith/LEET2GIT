2681f7822d9d66571fbb4b1f383f1c489365bb37
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-18 08:36:12 +0000

    set gihub id as unique

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class Users(Base):
     user_name = Column(String(255),nullable=False)
     email = Column(String(255),nullable=True,unique=True)
     avatar_url = Column(Text,nullable=True)
-    github_id = Column(BigInteger,nullable=False)
+    github_id = Column(BigInteger,nullable=False,unique=True)
     repo_name = Column(String(255), nullable=False, default='LeetCode')
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'), onupdate=text('now()'))
