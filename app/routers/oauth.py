1c128937c67aef96b4ff55dfda58be232e76f8e4
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-18 08:32:29 +0000

    takes repo_name as argument returns None if uniquekey Violation

--- app/routers/oauth.py
+++ app/routers/oauth.py
@@ -6,10 +6,12 @@ from pydantic import ValidationError
 from ..database import get_db
 from sqlalchemy.orm.session import Session
 from typing import Union
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import UniqueViolation
 
 GITHUB_API_URL = "https://api.github.com/user"
 
-def get_github_user(request:Request) -> schemas.GithubUser:
+def get_github_user(request:Request,token:str = None) -> schemas.GithubUser:
     """
     This function will return the user from the githug API
 
@@ -23,8 +25,8 @@ def get_github_user(request:Request) -> schemas.GithubUser:
         fastapi.HTTPException: If the token is invalid
     """
     logger.info("Verifying token")
-    token = request.cookies.get("access_token")
-    logger.info(f"Token: {token}")
+    if not token:
+        token = request.cookies.get("access_token")
     headers = {"Authorization": f"bearer {token}"}
     response = requests.get(GITHUB_API_URL, headers=headers)
     logger.info(f"Response: {response}")
@@ -32,23 +34,21 @@ def get_github_user(request:Request) -> schemas.GithubUser:
         logger.error("Invalid token")
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token",headers={"WWW-Authenticate": "Bearer"})
     logger.info("Token verified")
-    logger.info(f"User: {response.json()}")
     try:
         github_user = schemas.GithubUser(**response.json())
-        logger.info(f"Github user: {github_user}")
     except ValidationError as e:
         logger.error(f"Validation error: {e}")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to retrieve user")
     return github_user
 
 
-def get_current_user(request:Request,db:Session=Depends(get_db))-> Union[schemas.Users,schemas.GithubUser]:
+def get_current_user(github_id:int)-> Union[schemas.Users,schemas.GithubUser]:
     """
     This function will return the current user from the database.
     If the user is not present in the database then it will return None
     """
     logger.info("Getting current user")
-    github_id = request.cookies.get("github_id")
+    db:Session=next(get_db())
     user = db.query(models.Users).filter(models.Users.github_id == github_id).first()
     return user
     # if not user:
@@ -59,19 +59,19 @@ def get_current_user(request:Request,db:Session=Depends(get_db))-> Union[schemas
     # return user
 
 
-def create_user(github_user: schemas.GithubUser,db:Session)-> schemas.Users:
+def create_user(github_user: schemas.GithubUser,repo_name:str,db:Session)-> schemas.Users:
     """
     Creates a new user in the database
     """
     logger.info("Creating a new user")
     try:
-        new_user = models.Users(user_name=github_user.login,email=github_user.email,github_id=github_user.id,avatar_url=github_user.avatar_url)
+        new_user = models.Users(user_name=github_user.login,email=github_user.email,github_id=github_user.id,avatar_url=github_user.avatar_url,repo_name=repo_name)
         db.add(new_user)
         db.commit()
         logger.info(f"New user created: {new_user}")
-    except Exception as e:
+    except (IntegrityError,UniqueViolation) as e:
         logger.error(f"Failed to create a new user: {str(e)}")
-        return {"message":"User already exists"}
+        return None
     db.refresh(new_user)
     logger.info(f"New user: {new_user}")
     return schemas.Users.model_validate(new_user)
