d50af3f4f0fb52244ca963eb95e80d5e670e54bd
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-18 09:32:20 +0000

    removed save file feature and added query qenerator

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,17 +1,26 @@
 import os
 from scripts.logging_config import logger
 
-def save_file(folder_name:str,file_name:str,content:str):
-    """
-    Saves a file in the specified folder.
-    """
-    logger.info(f"Saving file {file_name} in {folder_name}")
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
+def query_generator(title_slug):
+    title_slug = title_slug.replace(" ","-").replace("/","")#The string should be free of space and "/"
     
-    file_path = os.path.join(folder_name,file_name)
-
-    with open(file_path,"w") as new_file:
-        new_file.write(content)
-    logger.info(f"File saved successfully at {file_path}")
+    query = f"""
+    query {{
+    question(titleSlug: "{title_slug}") {{
+    questionId
+    questionFrontendId
+    title
+    titleSlug
+    content
+    difficulty
+    topicTags {{
+    name
+    slug
+    translatedName
+    }}
+    }}
+    }}
+    """
+    logger.info("Query generated")
+    return query
     
