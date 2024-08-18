13b211d0efb0ef2651ad3d042c0e52e641c6f8a3
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-08-18 08:38:28 +0000

    Callback checks if user in db

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status,Request
+from fastapi import APIRouter, HTTPException, status,Request
 from fastapi.responses import RedirectResponse
-from ..database import get_db
 from .logging_config import logger  
 from ..utils import get_github_access_token
+from .oauth import get_github_user,get_current_user
+
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -15,7 +16,6 @@ async def get_github_callback(request:Request):
         logger.error("Invalid code")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Authorization code not found")
     logger.info("Code received")
-    logger.info(f"Code: {code}")
     # Exchange the authorization code for an access token
     token_response = get_github_access_token(code)
     # Set the access token in an HTTP-only cookie
@@ -25,8 +25,19 @@ async def get_github_callback(request:Request):
         value=token_response.access_token,
         httponly=True,  
         secure=True,    
-        max_age=3600,   # for 1 hour
+        max_age=3600,   # for 1 hourdf
     )
-    
+    github_user = get_github_user(request,token=token_response.access_token)
+    user = get_current_user(github_id=github_user.id)
+    if user:
+        response.set_cookie(
+            key="registered",
+            value="true",
+            httponly=False,
+            secure=True,
+            max_age=3600,
+        )
+        logger.info("User already registered")
+        return response
     logger.info("User authenticated and token set in cookie")
     return response
