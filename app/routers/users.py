c5eeef3cea3b30eaf01eee92200251855c2b39ac
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-10-19 10:36:28 +0000

    style: added comment for create user

--- app/routers/users.py
+++ app/routers/users.py
@@ -8,6 +8,20 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/api",status_code=status.HTTP_201_CREATED,response_model=schemas.Users)
 def create_user(folder_name:str="LeetCode",db:Session = Depends(get_db),user: dict = Depends(get_current_user)):
+    """
+    Creates a new user in the database.
+
+    Args:
+        folder_name (str): The name of the folder to be created for the user. Defaults to "LeetCode".
+        db (Session): The database session.
+        user (dict): The user details obtained from the authentication process.
+
+    Returns:
+        schemas.Users: The newly created user object.
+
+    Raises:
+        HTTPException: If the user already exists in the database.
+    """
     try:
         new_user = models.Users(user_name=user['login'],email=user['email'],github_id=user['id'],avatar_url=user['avatar_url'],folder_name=folder_name)
         db.add(new_user)
