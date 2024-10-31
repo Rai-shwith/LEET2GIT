517d06ac8603c1e887ba0698c83a930c71f2dc9c
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-10-31 13:02:44 +0530

    fix: accepts all the submissions from leetcode initially first 20 sumbissions were fetching because leetcode's api sends only 20 submission details at once so I looped till the has_next parameter in the api gets false. that is till i get all the submission

--- scripts/leetcode_solutions_fetcher.py
+++ scripts/leetcode_solutions_fetcher.py
@@ -1,16 +1,27 @@
 import requests
 from fastapi import HTTPException
-URL = 'https://leetcode.com/api/submissions/?offset=0&limit=4000&lastkey='
+BASE_URL = 'https://leetcode.com/api/submissions/'
 
 def leetcode_solution_fetcher(leetcode_access_token: str,csrftoken : str):
+    submissions = {"submissions_dump": []}
+    # Leetcode provides the solution in small chunks of 20, So this function uses while loop
     cookies = {
     'LEETCODE_SESSION': leetcode_access_token,
     'csrftoken': 'ozzJoQrSgQWUzJLMsmfhV2gGXv1H6rAgTC9PyrGAyL8olNx114p4u7vyYJvCECzK'
     }
-    # Make the GET request to the API endpoint with cookies
-    response = requests.get(URL, cookies = cookies)
-    # Check if the request was successful
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=400, detail="Failed to fetch the leetcode submissions")
+    last_key = "" # This is the key that will be used to fetch the next 20 solutions
+    while True:
+        # Make the GET request to the API endpoint with cookies
+        response = requests.get(f'{BASE_URL}?lastkey={last_key}', cookies = cookies)
+        data = response.json()
+        # Check if the response is successful
+        if response.status_code == 200:
+            # Append the fetched submissions to the submissions list
+            submissions["submissions_dump"].extend(data.get('submissions_dump', []))
+            last_key = data['last_key']
+            # check the end of submissions
+            if data["has_next"] == False:
+                break
+        else:
+            raise HTTPException(status_code=400, detail="Failed to fetch the leetcode submissions")
+    return submissions
