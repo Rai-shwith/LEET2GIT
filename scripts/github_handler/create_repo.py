0fb58879ea0f5bfcbc85105320c217604fdd6824
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-11-23 19:10:55 +0530

    refactor: improve the user's README message ,resolves #18

--- scripts/github_handler/create_repo.py
+++ scripts/github_handler/create_repo.py
@@ -17,7 +17,7 @@ def create_repo(user:AuthenticatedUser,repo_name:str,description:str="",private:
         if e.status==422:
             logger.info(f"Repository {repo_name} already exists")
             return None
-    initial_read_me_content :str = "This repository is created by LEET2GIT for storing LeetCode solutions.\n<!---LeetCode Topics Start-->\n<!---LeetCode Topics End-->\nDeveloped by <a href=\"https://github.com/Rai-shwith\">Rai-shwith</a>"
+    initial_read_me_content :str = "This repository is created by LEET2GIT for storing LeetCode solutions.\n<!---LeetCode Topics Start-->\n<!---LeetCode Topics End-->\n Organized using <a href=\"https://github.com/Rai-shwith/LEET2GIT\" target=\"_blank\">LEET2GIT</a> to sync and structure the LeetCode solutions.\n"
     upload_file(repo,"README.md",initial_read_me_content,f"Initial commit for {repo_name}")
     return repo
 
