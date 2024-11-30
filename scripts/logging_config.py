01b9958c06f083c19d1e19dc362693dae0a9568c
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-11-30 17:02:19 +0530

    chore: append the logging details instead of write helps to store previous logging information

--- scripts/logging_config.py
+++ scripts/logging_config.py
@@ -2,5 +2,5 @@ import logging
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s',
-                    handlers=[logging.FileHandler("LOGS/scripts.log","w"), logging.StreamHandler()])
+                    handlers=[logging.FileHandler("LOGS/scripts.log","a"), logging.StreamHandler()])
 logger = logging.getLogger(__name__)
