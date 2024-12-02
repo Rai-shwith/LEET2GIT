5d5e35714bd693f0a25b6e7a57b7092900f967dd
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-12-02 19:43:41 +0530

    fix: redundant processing of same solution, resolves #26

--- scripts/organize_leetcode_solutions.py
+++ scripts/organize_leetcode_solutions.py
@@ -50,16 +50,34 @@ async def organize_leetcode_solutions(raw_solutions: dict)->schemas.Uploads:
     logger.info("Organizing Leetcode solutions ...")
     
     submissions_dump = raw_solutions["submissions_dump"]
+    # unique_recent_submissions is for storing the most recent submission for each problem
+    unique_recent_submissions = {}
     uploads = []
     for submission in submissions_dump:
-        if submission["status_display"] == "Accepted":
-            title_slug = submission["title_slug"]
+        # Only consider accepted submissions
+        if submission["status_display"] != "Accepted":
+            continue
+        timestamp = submission["timestamp"]
+        title_slug = submission["title_slug"]
+        code_extension : str = get_extension(submission["lang_name"])
+        code : str = submission["code"]
+        solution : schemas.Solution = schemas.Solution(code=code,code_extension=code_extension)
+        
+        # if the submission is not in the unique_recent_submissions, add it directly 
+        if title_slug not in unique_recent_submissions:
             problemDetails : schemas.ProblemDetails = await get_problem_details(title_slug=title_slug)
-            code_extension : str = get_extension(submission["lang_name"])
-            code : str = submission["code"]
             solution : schemas.Solution = schemas.Solution(code=code,code_extension=code_extension)
             upload : schemas.Upload = schemas.Upload(question=problemDetails,solution=solution)
             uploads.append(upload)
+            unique_recent_submissions[title_slug] = {"timestamp":timestamp,"index":len(uploads)-1}
+        if title_slug in unique_recent_submissions and timestamp < unique_recent_submissions[title_slug]["timestamp"]:
+            continue
+        if title_slug in unique_recent_submissions and timestamp > unique_recent_submissions[title_slug]["timestamp"]:
+            problemDetails : schemas.ProblemDetails = await get_problem_details(title_slug=title_slug)
+            upload : schemas.Upload = schemas.Upload(question=problemDetails,solution=solution)
+            uploads[unique_recent_submissions[title_slug]["index"]] = upload
+            unique_recent_submissions[title_slug] = {"timestamp":timestamp,"index":len(uploads)-1}
+
     uploads : schemas.Uploads = schemas.Uploads(uploads=uploads)
     return uploads
             
