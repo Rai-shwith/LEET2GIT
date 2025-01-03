61a3bd298a5f56b9aacb59abf33543b2f523d958
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-01-03 22:12:58 +0530

    fix: logging file error

--- app/routers/logging_config.py
+++ app/routers/logging_config.py
@@ -1,8 +1,14 @@
 import logging
+import os
+
+# Ensure the LOGS directory exists
+log_dir = "LOGS"
+log_file = "oauth2.log"
+os.makedirs(log_dir, exist_ok=True)  # Create the directory if it does not exist
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s')
-file_handler = logging.FileHandler("LOGS/oauth2.log","a")
+file_handler = logging.FileHandler(os.path.join(log_dir, log_file),"a")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
--- scripts/logging_config.py
+++ scripts/logging_config.py
@@ -1,6 +1,19 @@
 import logging
-import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s',
-                    handlers=[logging.FileHandler("LOGS/scripts.log","a"), logging.StreamHandler()])
+import os
+
+# Ensure the LOGS directory exists
+log_dir = "LOGS"
+log_file = "scripts.log"
+os.makedirs(log_dir, exist_ok=True)  # Create the directory if it does not exist
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s',
+    handlers=[
+        logging.FileHandler(os.path.join(log_dir, log_file), mode="a"),
+        logging.StreamHandler()
+    ]
+)
 logger = logging.getLogger(__name__)
