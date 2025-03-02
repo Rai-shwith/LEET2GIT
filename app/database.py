794513304f6c94b86d19b684e50218335644b778
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-03-02 19:12:14 +0530

    made changes to ssl root certificate path and in db connection param

--- app/database.py
+++ app/database.py
@@ -8,15 +8,15 @@ import ssl
 SQL_ALCHEMY_DATABASE_URL = (
     f"{settings.database_protocol}+asyncpg://{settings.database_username}:"
     f"{settings.database_password}@{settings.database_host}:"
-    f"{settings.database_port}/{settings.database_host.split('.')[0]}.{settings.database_name}" #?sslmode={settings.database_connection_parameter}
+    f"{settings.database_port}/{settings.database_host.split('.')[0]}.{settings.database_name}?sslmode=require" #?sslmode={settings.database_connection_parameter}
 )
 
-ssl_context = ssl.create_default_context(cafile=settings.sslrootcert)
+# ssl_context = ssl.create_default_context(cafile=settings.sslrootcert)
 
 #create asynchronous engine
 engine = create_async_engine(
-    SQL_ALCHEMY_DATABASE_URL,
-    connect_args={"ssl":ssl_context}
+    SQL_ALCHEMY_DATABASE_URL
+    # connect_args={"ssl":ssl_context}
     )
 
 #create async session maker
