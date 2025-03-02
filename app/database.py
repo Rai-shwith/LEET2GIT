ec1ca988e121b8193ca8a0908b1736c8df92947a
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-03-02 21:54:55 +0530

    chore: remove ssl param from db_url

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ import ssl
 SQL_ALCHEMY_DATABASE_URL = (
     f"{settings.database_protocol}+asyncpg://{settings.database_username}:"
     f"{settings.database_password}@{settings.database_host}:"
-    f"{settings.database_port}/{settings.database_host.split('.')[0]}.{settings.database_name}?sslmode=require" #?sslmode={settings.database_connection_parameter}
+    f"{settings.database_port}/{settings.database_host.split('.')[0]}.{settings.database_name}" #?sslmode={settings.database_connection_parameter}
 )
 
 # ssl_context = ssl.create_default_context(cafile=settings.sslrootcert)
