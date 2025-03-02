b1d2b1efec038a72398acf0fdfe9df3a76ddc462
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-03-02 20:39:18 +0530

    chore: update log file path for heroku

--- app/routers/logging_config.py
+++ app/routers/logging_config.py
@@ -3,6 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s')
-file_handler = logging.FileHandler("LOGS/oauth2.log","a")
+file_handler = logging.FileHandler("../LOGS/oauth2.log","a")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
