1ee04bba40619febb3a184d1df3ac1940935f72c
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2025-03-08 14:53:20 +0530

    chore: represent pydantic models

--- app/schemas.py
+++ app/schemas.py
@@ -14,12 +14,22 @@ class Users(BaseModel):
     updated_at : datetime
     class Config:
         from_attributes = True  # Enable validation from ORM attributes
+    def __repr__(self):
+        return f"<Users id={self.id} user_name='{self.user_name}' github_id={self.github_id}>"
+
+    def __str__(self):
+        return self.__repr__()
 
 class GithubUser(BaseModel):
     id: int  # GitHub user ID
     login: str # GitHub username
     email: Union[EmailStr, None]
     avatar_url: str
+    def __repr__(self):
+        return f"<GitHubUser id={self.id} login='{self.login}'>"
+
+    def __str__(self):
+        return self.__repr__()
 
 class GitHubAccessTokenResponse(BaseModel):
     access_token: str
@@ -33,6 +43,10 @@ class ProblemDetails(BaseModel):
     questionId: str
     titleSlug: str
     topicTags: list[dict | None]
+    def __repr__(self):
+        return f"<ProblemDetails questionTitle={self.questionTitle} questionId={self.questionId}>"
+    def __str__(self):
+        return self.__repr__()
     
 class Solution(BaseModel):
     code_extension: str
